4f2244c46eee66bf4984d8b924f0ea46c5b42d4d
Ethan-Swanepoel <ST10198049@vcconnect.edu.za>
2024-09-29 15:13:26 +0200

    Update GamePortal
    Update GamePortal

--- app/src/main/java/com/example/practiceapplicationbrg/GamePortal.kt
+++ app/src/main/java/com/example/practiceapplicationbrg/GamePortal.kt
@@ -156,7 +156,7 @@ class GamePortal : AppCompatActivity() {
     private fun navigateToGameDetails(game: Game) {
         val intent = Intent(this, when (game.title) {
             "RetroSnake" -> SnakeGameActivity::class.java
-//            "Tic Tac Toe" -> TicTacToeActivity::class.java
+            "Tic Tac Toe" -> TikTakToeGameActivity::class.java
 //            "Hang Man" -> HangManActivity::class.java
             "Flappy Bird" -> FloppyBird_GameHub::class.java
             "Super Mystery Game" -> ActivityPlayersJournal::class.java
